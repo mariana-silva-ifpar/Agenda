a70204e97e5f62edb4d330125b509a11abfc7427
mariana-silva-ifpar <silva.mariana1@escolar.ifrn.edu.br>
2025-07-15 14:03:50 -0300

    feat: criação do projeto.

---
+++ src/app/adiciona-contato/adiciona-contato.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adiciona-contato',
+  imports: [],
+  templateUrl: './adiciona-contato.html',
+  styleUrl: './adiciona-contato.css'
+})
+export class AdicionaContato {
+
+}
